7e02bd99b5a199576dd8c5e2094aa84b9e6c4c9d
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-03-07 12:33:11 +0100

    Bumps akka-grpc and a snapshot of play-grpc

--- app/routers/HelloWorldRouter.scala
+++ app/routers/HelloWorldRouter.scala
@@ -1,12 +1,17 @@
 package routers
 
+import akka.actor.ActorSystem
 import akka.stream.Materializer
-import example.myapp.helloworld.grpc.{HelloReply, HelloRequest}
+import example.myapp.helloworld.grpc.{ HelloReply, HelloRequest }
 import javax.inject.Inject
 
 import scala.concurrent.Future
 
-class HelloWorldRouter @Inject() (mat: Materializer) extends example.myapp.helloworld.grpc.AbstractGreeterServiceRouter(mat) {
+class HelloWorldRouter @Inject() (
+                                   mat: Materializer,
+                                   system: ActorSystem
+                                 )
+  extends example.myapp.helloworld.grpc.AbstractGreeterServiceRouter(mat, system) {
   
   // We need to inject a Materializer since it is required by the abstract
   // router. It can also be used to access the ExecutionContext if you need
